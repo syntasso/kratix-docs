29de0991a6a8bf7fe54c56f86d58c0bd1ac179b0
Derik Evangelista <derik@syntasso.io>
2023-04-20 15:24:22 +0100

    chore: print query params

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -105,6 +105,7 @@ export function Marketplace(): JSX.Element {
   }
 
   const search = useLocation().search;
+  console.log(search);
   const showImages = new URLSearchParams(search).get("showImages") == "true"
 
   return (
