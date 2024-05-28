9668090ac503129c9f42ac8e6f5d26ea6e109ee6
Derik Evangelista <derik@syntasso.io>
2024-05-28 15:30:18 +0100

    feat: enable pipeline marketplace
    plus linting

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -1,30 +1,25 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import styles from './marketplace.module.scss';
-import Promises from '../../data/promise-data.json';
-import PipelineImages from '../../data/pipeline-image-data.json';
-import GridList from '@material-ui/core/GridList'; // TODO use @mui equivalent instead
-import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+import React from "react";
+import Layout from "@theme/Layout";
+import styles from "./marketplace.module.scss";
+import Promises from "../../data/promise-data.json";
+import PipelineImages from "../../data/pipeline-image-data.json";
+import GridList from "@material-ui/core/GridList"; // TODO use @mui equivalent instead
+import withWidth, { isWidthUp } from "@material-ui/core/withWidth";
 import {
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
   Chip,
-} from '@mui/material';
-import {
-  Breakpoint,
-  Theme,
-  styled,
-  useTheme,
-} from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import Link from '@docusaurus/Link';
-import clsx from 'clsx';
+} from "@mui/material";
+import { Breakpoint, Theme, styled, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
 type BreakpointOrNull = Breakpoint | null;
-import { useLocation } from '@docusaurus/router';
+import { useLocation } from "@docusaurus/router";
 
-const ListItem = styled('li')(({ theme }) => ({
+const ListItem = styled("li")(({ theme }) => ({
   margin: theme.spacing(0.5),
 }));
 
@@ -33,14 +28,24 @@ function Banner({ title, style }) {
     <div className={clsx(styles.banner, style)}>
       <h3 className={styles.bannerTitle}>{title}</h3>
     </div>
-  )
+  );
 }
 
-function MarketplaceCard({ name, url, logoUrl, description, categories, available = true, example = false }) {
+function MarketplaceCard({
+  name,
+  url,
+  logoUrl,
+  description,
+  categories,
+  available = true,
+  example = false,
+}) {
   return (
-    <Card className={clsx(styles.card, available ? null : styles.cardComingSoon)}>
+    <Card
+      className={clsx(styles.card, available ? null : styles.cardComingSoon)}
+    >
       <CardActionArea
-        href={url || '#'}
+        href={url || "#"}
         disabled={!available}
         target="_blank"
         className={styles.cardAction}
@@ -55,23 +60,27 @@ function MarketplaceCard({ name, url, logoUrl, description, categories, availabl
 
         <CardContent>
           <h2 className={styles.cardTitle}>{name}</h2>
-          <p className={styles.cardDescription}>
-            {description}
-          </p>
+          <p className={styles.cardDescription}>{description}</p>
 
           <ul className={styles.categoriesList}>
             {categories.map((category) => (
               <ListItem key={category}>
-                <Chip variant="outlined" label={category} className={styles.chip} />
+                <Chip
+                  variant="outlined"
+                  label={category}
+                  className={styles.chip}
+                />
               </ListItem>
             ))}
           </ul>
-          {!available ? <Banner title="Coming Soon" style={styles.preview} /> : null}
+          {!available ? (
+            <Banner title="Coming Soon" style={styles.preview} />
+          ) : null}
           {example ? <Banner title="Example" style={styles.example} /> : null}
         </CardContent>
       </CardActionArea>
     </Card>
-  )
+  );
 }
 
 function useWidth() {
@@ -82,35 +91,27 @@ function useWidth() {
       // eslint-disable-next-line react-hooks/rules-of-hooks
       const matches = useMediaQuery(theme.breakpoints.up(key));
       return !output && matches ? key : output;
-    }, null) || 'xs'
+    }, null) || "xs"
   );
 }
 
 export function Marketplace(): JSX.Element {
   const getGridListCols = () => {
     const width = useWidth();
-    if (isWidthUp('xl', width)) {
+    if (isWidthUp("xl", width)) {
       return 4;
     }
 
-    if (isWidthUp('lg', width)) {
+    if (isWidthUp("lg", width)) {
       return 3;
     }
 
-    if (isWidthUp('md', width)) {
+    if (isWidthUp("md", width)) {
       return 2;
     }
 
     return 1;
-  }
-
-  const search = useLocation().search;
-  const showImages = new URLSearchParams(search).get("pipeline") == "true"
-
-  let pipelineMktClass = "pipelineSection"
-  if (!showImages) {
-    pipelineMktClass = "hidden"
-  }
+  };
 
   return (
     <Layout
@@ -121,11 +122,21 @@ export function Marketplace(): JSX.Element {
         <header className={"text--center"}>
           <hgroup>
             <h1>Kratix Marketplace</h1>
-            <p className="text--center">Community Promises for <Link href="/">Kratix</Link>, the framework for building platforms.</p>
+            <p className="text--center">
+              Community Marketplace with <Link href="#promises">Promises</Link>{" "}
+              and <Link href="#images">Pipeline images</Link> for{" "}
+              <Link href="/">Kratix</Link>, the framework for building
+              platforms.
+            </p>
           </hgroup>
         </header>
+
         <div className="root">
-          <section>
+          <section id="promises" className={styles.pipelineMarketplace}>
+            <hgroup className="text--center">
+              <h2>Promises</h2>
+              <p className="text--center"></p>
+            </hgroup>
             <GridList
               cellHeight={"auto"}
               className={styles.gridList}
@@ -140,10 +151,10 @@ export function Marketplace(): JSX.Element {
             </GridList>
           </section>
 
-          <section className={pipelineMktClass}>
+          <section id="images" className={styles.pipelineMarketplace}>
             <hgroup className="text--center">
               <h2>Pipeline Images</h2>
-              <p className="text--center">Community Pipeline images for <Link href="/main/reference/promises/intro">Kratix Promises</Link>.</p>
+              <p className="text--center"></p>
             </hgroup>
 
             <GridList
@@ -160,7 +171,10 @@ export function Marketplace(): JSX.Element {
             </GridList>
           </section>
         </div>
-        <div className={clsx('text--center', 'margin-top--xl', styles.bottomText)}>
+
+        <div
+          className={clsx("text--center", "margin-top--xl", styles.bottomText)}
+        >
           <p>
             Want to contribute a Promise to the Marketplace?
             <Link href="/marketplace/contributing">Check out our guide.</Link>
@@ -168,7 +182,9 @@ export function Marketplace(): JSX.Element {
 
           <p>
             Do you have any feedback? Did not find what you were looking for?
-            <Link href="https://github.com/syntasso/kratix-marketplace/issues/new/choose">Submit an issue for it on GitHub</Link>
+            <Link href="https://github.com/syntasso/kratix-marketplace/issues/new/choose">
+              Submit an issue for it on GitHub
+            </Link>
           </p>
         </div>
       </main>
