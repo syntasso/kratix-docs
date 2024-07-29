b384743db40f4daf8f109a54b3a49390c829868a
Derik Evangelista <derik@syntasso.io>
2024-07-29 15:29:55 +0100

    feat(#86): use kratix cli on workshop

--- docs/workshop/part-ii/_partials/secrets-and-state/02-create-bucket.sh
+++ docs/workshop/part-ii/_partials/secrets-and-state/02-create-bucket.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -euxo pipefail
 
 name=$(yq '.metadata.name' /kratix/input/object.yaml)
 namespace=$(yq '.metadata.namespace' /kratix/input/object.yaml)
 
-cd /terraform
+cd resources
 terraform init
 terraform apply -auto-approve --var="bucket_name=${name}.${namespace}"
