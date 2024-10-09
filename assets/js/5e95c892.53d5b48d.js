f6236e8e404c796f9e4f9d295ae47ecbe1196ae0
syntassodev <kratix@syntasso.io>
2024-10-09 13:19:56 +0000

    [ci skip] Deploy website - based on 1d414d61572a504056b402cd614ad6d4312f1f09

--- assets/js/5e95c892.53d5b48d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkratix_docs=self.webpackChunkkratix_docs||[]).push([[9661],{1892:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});r(7294);var a=r(6905),c=r(1667),t=r(5281),u=r(8790),d=r(7372),k=r(5893);function n(e){return(0,k.jsx)(c.FG,{className:(0,a.Z)(t.k.wrapper.docsPages),children:(0,k.jsx)(d.Z,{children:(0,u.H)(e.route.routes)})})}}}]);
