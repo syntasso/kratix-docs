110dddd466bf773306b413c869dd1e1f890acf63
Derik Evangelista <derik@syntasso.io>
2024-11-22 11:46:54 +0000

    fix: stop scarf pixel taking 500x500px 😅

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -12,20 +12,24 @@ export default function FooterWrapper(props: Props): JSX.Element {
   const isBlog = location.pathname.startsWith('/blog');
   const isSKE = location.pathname.startsWith('/ske');
   const isDocs = !isSKE && !isBlog;
+  const styles = {
+    height: '1px',
+    width: '1px',
+  };
 
   return (
     <>
       {isDocs && (
-        <img id="docs-scarf-pixel" referrerPolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=2f7703a4-9013-4c6f-b09e-894a5f3f42d6" />
+        <img id="docs-scarf-pixel" style={styles} referrerPolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=2f7703a4-9013-4c6f-b09e-894a5f3f42d6" />
       )}
 
       {isSKE && (
-        <img id="ske-scarf-pixel" referrerPolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=f09f95ff-0179-40f1-a33c-826b0e29c3de" />
+        <img id="ske-scarf-pixel" style={styles} referrerPolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=f09f95ff-0179-40f1-a33c-826b0e29c3de" />
       )}
 
       {isBlog && (
         // "Blog"
-        <img id="blog-scarf-pixel" referrerPolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=03fbea28-52ec-43c2-90d3-519166e436b4" />
+        <img id="blog-scarf-pixel" style={styles} referrerPolicy="no-referrer-when-downgrade" src="https://static.scarf.sh/a.png?x-pxid=03fbea28-52ec-43c2-90d3-519166e436b4" />
       )}
       <Footer {...props} />
     </>
