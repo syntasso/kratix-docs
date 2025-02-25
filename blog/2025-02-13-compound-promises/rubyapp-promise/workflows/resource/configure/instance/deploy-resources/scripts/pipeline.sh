d32ef6ee35e5603cc5df099e3c24c524de3eb76c
Derik Evangelista <derik@syntasso.io>
2025-02-25 09:33:12 +0000

    wip: most of the blog
    missing a diagram

--- blog/2025-02-13-compound-promises/rubyapp-promise/workflows/resource/configure/instance/deploy-resources/scripts/pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -xe
-
-name="$(yq eval '.metadata.name' /kratix/input/object.yaml)"
-namespace=$(yq '.metadata.namespace' /kratix/input/object.yaml)
-
-echo "Hello from ${name} ${namespace}"
