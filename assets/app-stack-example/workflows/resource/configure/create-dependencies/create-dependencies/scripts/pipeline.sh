6dba7293add5ccf74abe8d52054cfac1503a7bf6
Stella Englezou <9325243+senglezou@users.noreply.github.com>
2026-03-26 17:04:42 +0000

    wip: uodate docs to reflect the three pipelines design

--- assets/app-stack-example/workflows/resource/configure/create-dependencies/create-dependencies/scripts/pipeline.sh
+++ assets/app-stack-example/workflows/resource/configure/create-dependencies/create-dependencies/scripts/pipeline.sh
@@ -7,31 +7,8 @@ app_namespace="$(yq eval '.metadata.namespace // "default"' /kratix/input/object
 
 mkdir -p /kratix/output /kratix/metadata
 
-bucket_requested=false
+
 database_requested=false
-
-bucket_name="$(yq eval '.spec.bucket.name // ""' /kratix/input/object.yaml)"
-bucket_public="$(yq eval '.spec.bucket.public // false' /kratix/input/object.yaml)"
-
-if [ -n "${bucket_name}" ]; then
-  bucket_requested=true
-
-  cat > /kratix/output/bucket-request.yaml <<EOF
-apiVersion: example.kratix.io/v1alpha1
-kind: Bucket
-metadata:
-  name: ${bucket_name}
-  namespace: ${app_namespace}
-  labels:
-    kratix.io/component-of-promise-name: app-stack
-    kratix.io/component-of-resource-name: ${app_name}
-    kratix.io/component-of-resource-namespace: ${app_namespace}
-spec:
-  bucketName: ${bucket_name}
-  public: ${bucket_public}
-EOF
-fi
-
 
 database_driver="$(yq eval '.spec.database.driver // ""' /kratix/input/object.yaml)"
 database_name="${app_name}-db"
@@ -53,6 +30,29 @@ spec:
   env: dev
   teamId: ${app_name}
   dbName: ${database_name}
+EOF
+fi
+
+bucket_requested=false
+bucket_name="$(yq eval '.spec.bucket.name // ""' /kratix/input/object.yaml)"
+bucket_public="$(yq eval '.spec.bucket.public // false' /kratix/input/object.yaml)"
+
+if [ -n "${bucket_name}" ]; then
+  bucket_requested=true
+
+  cat > /kratix/output/bucket-request.yaml <<EOF
+apiVersion: example.kratix.io/v1alpha1
+kind: Bucket
+metadata:
+  name: ${bucket_name}
+  namespace: ${app_namespace}
+  labels:
+    kratix.io/component-of-promise-name: app-stack
+    kratix.io/component-of-resource-name: ${app_name}
+    kratix.io/component-of-resource-namespace: ${app_namespace}
+spec:
+  bucketName: ${bucket_name}
+  public: ${bucket_public}
 EOF
 fi
 
